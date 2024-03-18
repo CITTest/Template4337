5d9e8fa95334f74cae964ec9a7d3621eff6caf3c
Mokrovi <xxantonxx21@mail.ru>
2024-03-18 17:20:31 +0300

    надо подключить exel и проверить подключение к бд

---
+++ Template4337/Template4337/Class1.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Template4337
+{
+    public class class1
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string View { get; set; }
+        public string Code { get; set; }
+        public int Price { get; set; }
+        public int Group { get; set; }
+
+        public class1(string  name, string view, string code, int price)
+        {
+            Name = name;
+            View = view;
+            Code = code;
+            Price = price;
+
+            if (Price < 351) Group = 1;
+            if (Price > 350 && Price < 801) Group = 2;
+            if (Price > 800) Group = 3;
+        }
+    }
+    public partial class Context : DbContext
+    {
+        public Context()
+        {
+        }
+
+        public Context(DbContextOptions<Context> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<class1> Class1s { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=isrpo3;Trusted_Connection=True;");
+            }
+        }
+    }
+    }
--- Template4337/Template4337/Yakovlev_4337.xaml.cs
+++ Template4337/Template4337/Yakovlev_4337.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,8 +10,6 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace Template4337
 {
@@ -22,6 +21,117 @@ namespace Template4337
         public Yakovlev_4337()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Import
+
+            var openFileDialog = new OpenFileDialog()
+            {
+                DefaultExt = "*.xls; *xlsx",
+                Title = "Выберите файлы excel для импорта в базу данных",
+            };
+
+            var result = openFileDialog.ShowDialog();
+
+            if (!result.HasValue || !result.Value)
+                return;
+
+            /*var excelWork = new Excel.Application();
+            var ServWork = excelWork.Workbooks.Open(openFileDialog.FileName);
+
+            var ServWorkSheet = (Excel.Worksheet)ServWork.Sheets[1];
+            var lastCell = ServWorkSheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell);
+            var columns = lastCell.Column;
+            var rows = ServWorkSheet.Cells[ServWorkSheet.Rows.Count, 1].End(-4162).Row;
+
+            var list = new string[rows, columns];
+
+            for (var i = 0; i < columns; i++)
+                for (var j = 0; j < rows; j++)
+                    list[j, i] = ServWorkSheet.Cells[j + 1, i + 1].Text;
+
+            var Services = new List<class1>();
+            MessageBox.Show($"{rows}");
+
+            for (var i = 2; i < rows; i++)
+            {
+                var temp = new class1(list[i, 1], list[i, 2], list[i, 3], Convert.ToInt32(list[i, 4]));
+
+               
+
+                Services.Add(temp);
+            }
+            try
+            {
+                using (var context = new Context())
+                {
+                    context.Class1s.AddRange(Services);
+                    context.SaveChanges();
+                }
+                MessageBox.Show($"Добавление в базу данных прошло успешно {Services.Count}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка базы данных {ex.Message}");
+            }*/
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            const int idCol = 1;
+            const int Name = 2;
+            const int View = 3;
+            const int Code = 4;
+            const int Price = 5;
+
+            using (var context = new Context())
+            {
+                var status = context.Class1s.GroupBy(p => p.Group).Select(p => p.Key).ToList();
+
+                /*var app = new Excel.Application();
+                app.SheetsInNewWorkbook = 3;
+                var workbook = app.Workbooks.Add(Type.Missing);
+
+                for (var i = 0; i < 3; i++)
+                {
+                    var worksheet = app.Worksheets.Item[i + 1];
+                    worksheet.Name = "Page - " + i;
+
+                    var startIndexRow = 2;
+
+                    worksheet.Cells[idCol][1] = "Id";
+                    worksheet.Cells[Name][1] = "Название";
+                    worksheet.Cells[View][1] = "Вид услуги";
+                    worksheet.Cells[Code][1] = "Код услуги";
+                    worksheet.Cells[Price][1] = "Цена";
+
+                    var orderThisStatus = context.Class1s.Where(p => p.Group == i);
+                    foreach (var item in orderThisStatus)
+                    {
+                        worksheet.Cells[idCol][startIndexRow] = item.Id;
+                        worksheet.Cells[Name][startIndexRow] = item.Name;
+                        worksheet.Cells[View][startIndexRow] = item.View;
+                        worksheet.Cells[Code][startIndexRow] = item.Code;
+                        worksheet.Cells[Price][startIndexRow] = item.Price;
+
+                        startIndexRow++;
+                    }
+                }
+
+                app.Visible = true;*/
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
